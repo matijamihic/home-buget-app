7621760387ae6bb3388e823f68d20020b48dc914
Matija Mihic <matija@littleemperors.com>
2023-05-17 21:52:30 +0200

    feat (DbSeed) : improved testing dataset

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,33 +17,130 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        
+        $incomeDescriptions = [
+            ['Salary', 2000],
+            ['Freelance Work', 1000],
+            ['Investment Income', 500],
+            ['Rental Income', 800],
+            ['Other', 300]
+        ];
+        
+        $startDate = Carbon::now()->subYear();
+        $endDate = Carbon::now();
+        
+        $currentDate = $startDate->copy();
+        $incomeSum = 0;
+        
+        while ($currentDate <= $endDate) {
+            foreach ($incomeDescriptions as $description) {
+                $incomeAmount = rand($description[1] - 100, $description[1] + 100);
+        
+                \App\Models\Income::factory()->create([
+                    'user_id' => 1,
+                    'amount' => $incomeAmount,
+                    'description' => $description[0],
+                    'date' => $currentDate->copy(),
+                ]);
+        
+                $incomeSum += $incomeAmount;
+            }
+        
+            $currentDate->addMonth();
+        }
 
-        \App\Models\Income::factory()->create([
-            'user_id' => 1,
-            'amount' => 2000,
-            'description' => 'Salary',
-            'date' => \Carbon\Carbon::now()->subDays(rand(1, 30)),
-        ]);
-
-        $categories = ['bills', 'rent', 'loans', 'car', 'food', 'entertainment', 'clothing', 'health', 'travel', 'other' ];
-        foreach ($categories as $expenseType) {
+        $categories = [
+            'bills' => 100,
+            'rent' => 500,
+            'loans' => 200,
+            'car' => 300,
+            'food' => 150,
+            'entertainment' => 50,
+            'clothing' => 75,
+            'health' => 80,
+            'travel' => 200,
+            'other' => 100,
+        ];
+        
+        foreach ($categories as $expenseType => $averageAmount) {
             \App\Models\ExpenseCategory::factory()->create([
                 'name' => $expenseType,
-            ]);        
-        } 
-
-        $descriptions = [['Electricity', 'bills'], ['Water', 'bills'], ['Gas', 'bills'], ['Internet', 'bills'], ['Phone', 'bills'], ['Rent', 'rent'], ['Car Payment', 'car'], ['Car Insurance', 'car'], ['Car Maintenance', 'car'], ['Groceries', 'food'], ['Eating Out', 'food'], ['Movies', 'entertainment'], ['Concerts', 'entertainment'], ['Clothes', 'clothing'], ['Shoes', 'clothing'], ['Doctor', 'health'], ['Dentist', 'health'], ['Medicine', 'health'], ['Airfare', 'travel'], ['Hotel', 'travel'], ['Rental Car', 'travel'], ['Gas', 'travel'], ['Other', 'other']];
-        $expenses_sum = 0;
-        foreach ($descriptions as $description) {
-            $expense = rand(10, 100);
-            \App\Models\Expense::factory()->create([
-                'user_id' => 1,
-                'expense_category_id' => \App\Models\ExpenseCategory::where('name', $description[1])->first()->id,
-                'amount' => $expense,
-                'description' => $description[0],
-                'date' => \Carbon\Carbon::now()->subDays(rand(1, 30)),
             ]);
-            $expenses_sum += $expense;
+        }
+        
+        $startDate = Carbon::now()->subYear();
+        $endDate = Carbon::now();
+        
+        $currentDate = $startDate->copy();
+        $expenses_sum = 0;
+        
+        // Descriptions for recurring expenses
+        $recurringDescriptions = [
+            ['Electricity', 'bills'],
+            ['Water', 'bills'],
+            ['Gas', 'bills'],
+            ['Internet', 'bills'],
+            ['Phone', 'bills'],
+            ['Rent', 'rent'],
+            ['Car Payment', 'car'],
+        ];
+        
+        // Descriptions for other expenses
+        $otherDescriptions = [
+            ['Car Insurance', 'car'],
+            ['Car Maintenance', 'car'],
+            ['Groceries', 'food'],
+            ['Eating Out', 'food'],
+            ['Movies', 'entertainment'],
+            ['Concerts', 'entertainment'],
+            ['Clothes', 'clothing'],
+            ['Shoes', 'clothing'],
+            ['Doctor', 'health'],
+            ['Dentist', 'health'],
+            ['Medicine', 'health'],
+            ['Airfare', 'travel'],
+            ['Hotel', 'travel'],
+            ['Rental Car', 'travel'],
+            ['Gas', 'travel'],
+            ['Other', 'other']
+        ];
+        
+        while ($currentDate <= $endDate) {
+            // Generate recurring expenses for each month
+            foreach ($recurringDescriptions as $description) {
+                $category = \App\Models\ExpenseCategory::where('name', $description[1])->first();
+                $averageAmount = $categories[$description[1]];
+                $expense = rand($averageAmount - 50, $averageAmount + 50);
+        
+                \App\Models\Expense::factory()->create([
+                    'user_id' => 1,
+                    'expense_category_id' => $category->id,
+                    'amount' => $expense,
+                    'description' => $description[0],
+                    'date' => $currentDate->copy(),
+                ]);
+        
+                $expenses_sum += $expense;
+            }
+        
+            // Generate other expenses with random dates
+            foreach ($otherDescriptions as $description) {
+                $category = \App\Models\ExpenseCategory::where('name', $description[1])->first();
+                $averageAmount = $categories[$description[1]];
+                $expense = rand($averageAmount - 50, $averageAmount + 50);
+        
+                \App\Models\Expense::factory()->create([
+                    'user_id' => 1,
+                    'expense_category_id' => $category->id,
+                    'amount' => $expense,
+                    'description' => $description[0],
+                    'date' => $currentDate->copy()->addDays(rand(1, 30)),
+                ]);
+        
+                $expenses_sum += $expense;
+            }
+        
+            $currentDate->addMonth();
         }
  
         $balance = 2000 - $expenses_sum;
